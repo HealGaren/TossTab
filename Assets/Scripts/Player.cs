528512a7a3878797f7f8b99abacfdff2f87e0e59
HealGaren <qkswkd7003@naver.com>
2016-09-19 10:33:37 +0900

    네트워크 서버 / 뷰 추가

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+    NetworkView view;
+    Rigidbody2D rigidbody2d;
+
+    void Start()
+    {
+        view = GetComponent<NetworkView>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if (view.isMine)
+        {
+            InputMovement();
+        }
+    }
+
+    void InputMovement()
+    {
+
+        Vector2 vec = transform.position;
+            if (Input.GetKey(KeyCode.W))
+                rigidbody2d.MovePosition(rigidbody2d.position + Vector2.up * speed * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.S))
+                rigidbody2d.MovePosition(rigidbody2d.position + Vector2.down * speed * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.D))
+                rigidbody2d.MovePosition(rigidbody2d.position + Vector2.right * speed * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.A))
+                rigidbody2d.MovePosition(rigidbody2d.position + Vector2.left * speed * Time.deltaTime);
+    }
+}
